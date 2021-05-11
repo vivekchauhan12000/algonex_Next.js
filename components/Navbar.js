196c81856262f47b5ee56e59e365b081fa47e787
vivekchauhan12000 <vivekchauhan12000@gmail.com>
2021-05-11 17:16:44 +0530

    navbar added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar=()=>{
   return(
     <nav>
       <div className="logo">
         <h2>Algonex</h2>
         </div>
-        <a>Home</a>
-        <a>about</a>
-        <a>questions</a>
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/about"><a>about</a></Link>
+        <Link href="/question"><a>questions</a></Link>
 
     </nav>
   );
---
+++ pages/question.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+
+const question=()=>{
+
+ return(
+   <div>
+     <Navbar/>
+   <h1>Questions</h1>
+   <Footer/>
+   </div>
+ );
+
+}
+export default question ;
